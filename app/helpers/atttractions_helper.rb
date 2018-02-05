2a3847a5fec188cbbd13660d60fcc0879faadfc5
Erin Land <erinland05@gmail.com>
2018-02-05 20:29:23 +0000

    Done.

---
+++ app/helpers/atttractions_helper.rb
@@ -0,0 +1,2 @@
+module AtttractionsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
+root "static_pages#home"
 
+get "/signin", to: "sessions#new"
+post "/sessions/create", to: "sessions#create"
+get '/signout', to: "sessions#destroy"
+post "/rides/new", to: "rides#new"
+
+resources :users
+resources :attractions
 end
